718a755597e0cc751dba53effd2fd6602b689d67
kricn <mnyt2436@126.com>
2022-11-07 10:09:35 +0800

    调整图表x轴多的情况下会放大

--- mock/dict.ts
+++ mock/dict.ts
@@ -1,5 +1,5 @@
 export default {
-  '/versions': [
+  '/versions/fusion': [
     'versions1',
     'versions2',
     'versions3',
@@ -14,11 +14,11 @@ export default {
     'versions3',
   ],
 
-  '/metrics': [1, 2, 3, 4, 5],
+  '/metrics/fusion': [1, 2, 3, 4, 5],
 
-  '/cases': [6, 7, 8, 9, 10],
+  '/cases/fusion': [6, 7, 8, 9, 10],
 
-  '/calculate': [11, 12, 13, 14],
+  '/calculate/fusion': [11, 12, 13, 14],
 
   'POST /send': {
     versions: [
@@ -34,9 +34,14 @@ export default {
       'versions1',
       'versions2',
       'versions3',
+      'versions2',
+      'versions3',
+      'versions1',
+      'versions2',
+      'versions3',
     ],
-    metrics: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
-    cases: [6, 7, 8, 9, 10, 6, 7, 8, 9, 10],
-    calculate: [11, 12, 13, 14, 11, 12, 13, 14],
+    metrics: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+    cases: [6, 7, 8, 9, 10, 6, 7, 8, 9, 10, 6, 7, 8, 9, 10, 6, 7, 8, 9, 10],
+    calculate: [11, 12, 13, 14, 11, 12, 13, 14, 11, 12, 13, 14, 11, 12, 13, 14],
   },
 };
